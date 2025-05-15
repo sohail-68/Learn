14e59cc11b25cc23f9e72559d6f5aa624deec842
sohail <sohailkhanas23as23.com>
2025-05-15 23:58:53 +0530

    nextt commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-// const quizRoutes = require('./routes/quizRoutes.js');
+const quizRoutes = require('./routes/quiz.js');
 const authRoutes = require('./routes/authRoutes.js');
 const lecture = require('./routes/lecture.js');
 const courseRoutes = require('./routes/courseRoutes');
-// const progrroutes = require('./routes/progressRoutes.js');
+const progrroutes = require('./routes/progress.js');
 const path = require('path');
 const app = express();
 connectDB();
@@ -18,8 +18,8 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', authRoutes);
 app.use('/lecture', lecture);
-// app.use('/api/quizzes', quizRoutes);
-// app.use('/progress', progrroutes);
+app.use('/api/quizzes', quizRoutes);
+app.use('/progress', progrroutes);
 app.use('/api/courses', courseRoutes);
 
 const PORT = process.env.PORT || 5000;
